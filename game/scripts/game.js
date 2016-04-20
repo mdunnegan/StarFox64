c55c6538acb4b1dcad317d685353c54d5a7c2b20
Michael Dunnegan <mdunnegan@spicy.io>
2016-04-20 15:00:00 -0400

    Added bump map and texture to terrain, randomized placement of obstacles

--- game/scripts/game.js
+++ game/scripts/game.js
@@ -53,6 +53,10 @@ var game = function(){
 	    light.position.set( 0, 1, 1 ).normalize();
 	    scene.add(light);
 
+	    var secondLight = new THREE.DirectionalLight( 0xff99ff );
+	    camera.add(secondLight);
+
+
 	    // TERRAIN GENERATOR
 		terrainGenerator = new TerrainGenerator();
 		terrainGenerator.generateInitialTiles(scene);
@@ -78,6 +82,7 @@ var game = function(){
 
 	var updateCameraAndShipPositionOnZAxis = function(){
 		camera.position.z -= 1;
+		//camera.rotation.y = 90;
 		ship.getModel().position.z -= 1;
 	};
 
@@ -88,6 +93,22 @@ var game = function(){
 	})();
 
 };
+
+// function initTextures() {
+//   cubeTexture = gl.createTexture();
+//   cubeImage = new Image();
+//   cubeImage.onload = function() { handleTextureLoaded(cubeImage, cubeTexture); }
+//   cubeImage.src = "cubetexture.png";
+// }
+
+function handleTextureLoaded(image, texture) {
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+  gl.generateMipmap(gl.TEXTURE_2D);
+  gl.bindTexture(gl.TEXTURE_2D, null);
+}
 
 function CollisionDetector(shipMesh, terrainList, obstacleList){
 	this.ship = shipMesh;
@@ -117,7 +138,10 @@ ObstacleGenerator.prototype = {
 		// 'i' will be an index for choosing objects
 		var i = 0;
 		obstacle = new THREE.Mesh(this.obstacleGeometries[i], this.obstacleMaterials[i]);
-		obstacle.position.x = 0;
+		//obstacle.position.x = // random between -400 and 0?
+
+		obstacle.position.x = (Math.random() * 200) - 100;
+
 		obstacle.position.y = 5;
 		obstacle.position.z = -zCoordinateOfTile;
 
@@ -132,9 +156,10 @@ ObstacleGenerator.prototype = {
 function TerrainGenerator(){
 	this.zRenderingPosition = 0;
 	this.activeTerrain = [];
+	this.sizeOfTerrain = 100;
 	this.obstacleGenerator = new ObstacleGenerator();
 
-	this.terrainGeometry = new THREE.CubeGeometry(100, 10, 100);
+	this.terrainGeometry = new THREE.CubeGeometry(10, 10, 1);
 	this.terrainMaterial = new THREE.MeshPhongMaterial( { map: THREE.ImageUtils.loadTexture('textures/sand.jpg') } );
 
 	this.loader = new THREE.JSONLoader();
@@ -166,7 +191,7 @@ TerrainGenerator.prototype = {
 	},
 
 	nextTileNeeded: function(ZpositionOfShip){
-		if (ZpositionOfShip % 100 == 0){
+		if (ZpositionOfShip % this.sizeOfTerrain == 0){
 			return true;
 		}
 	},
@@ -175,29 +200,41 @@ TerrainGenerator.prototype = {
 
 		var START = -200;
 		var END = 200;
-		var TILE_SIZE = 100;
 
-	    for (var i = -200; i <= 200; i+=100){
+	    for (var i = START; i <= END; i+=100){
 
 	    	var AT = this.getActiveTerrain();
 	    	var zPos = this.zRenderingPosition;
 
 	  		this.loader.load(
 				// resource URL
-				'textures/ruggedTerrain.json',
+				'textures/ruggedTerrain3.json',
 
 				// Function when resource is loaded
-				function ( geometry, material ) {
-					var object = new THREE.Mesh( geometry, material );
-					object.position.y = -150;
-					object.position.z = -zPos;
-					object.position.x = i-400;
-					object.scale.set(100,100,2);
+				function (geometry) {
 
-					AT.push(object);
+					var material = new THREE.MeshPhongMaterial({
+						color:0xffffff, 
+						map: THREE.ImageUtils.loadTexture( 'textures/sand.jpg'),
+						bumpMap: THREE.ImageUtils.loadTexture('textures/sandBumpMap.jpg'),
+						bumpScale: 0.5
+					});
+
+					var object1 = new THREE.Mesh(geometry, material);
+
+					object1.position.y = -150;
+					object1.position.z = -zPos-400;
+					object1.position.x = i-400; // i - a random number between 0 and 200
+					object1.position.x = i - (Math.random() * 400) + 100;
+
+					object1.scale.set(100,100,100);
+
+					object1.rotation.y = (Math.floor(Math.random() * 4) + 1) * 90; 
+
+					AT.push(object1);
 					AT.shift();
 					
-					scene.add(object);
+					scene.add(object1);
 				}
 			);
 	    }
@@ -231,7 +268,7 @@ Spaceship.prototype = {
 
 	setInitialPosition: function(){
 		this.model.position.z = -10;
-    	this.model.position.y = 10;
+    	this.model.position.y = 25;
 	},
 
 	applyTexture: function(){
@@ -243,15 +280,20 @@ Spaceship.prototype = {
 	handleInput: function(){
 		if (Key.isDown(Key.A)){
 			this.model.position.x -= 1;
+			this.model.rotation.z += .01;
+			// make the camera move down too?
 		}
 		if (Key.isDown(Key.D)) {
 			this.model.position.x += 1;
+			this.model.rotation.z -= .01;
 		} 
 		if (Key.isDown(Key.S)) {
 			this.model.position.y -= 1;
+			this.model.rotation.x -= .01;
 	   	} 
 	   	if (Key.isDown(Key.W)) {
 			this.model.position.y += 1;
+			this.model.rotation.x += .01;
 		}
 	}
 }
